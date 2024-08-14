c9d8a47e296e486cde21b2a89e876934552fc6a5
cat394 <keyring1231@gmail.com>
2024-08-14 13:13:11 +0900

    chore: format code

--- packages/middlewares/cors/src/types.ts
+++ packages/middlewares/cors/src/types.ts
@@ -1,7 +1,7 @@
 /**
  * HTTP methods that are typically allowed in CORS requests.
  */
-export type BaseMethods = 'GET' | 'HEAD' | 'PUT' | 'PATCH' | 'POST' | 'DELETE';
+export type BaseMethods = "GET" | "HEAD" | "PUT" | "PATCH" | "POST" | "DELETE";
 
 /**
  * The result of an origin validation function, indicating whether the origin is allowed.
@@ -14,7 +14,7 @@ type OriginFunctionResult = boolean | void;
  * @returns A boolean indicating if the origin is allowed, or a promise that resolves to a boolean.
  */
 type OriginFunction = (
-	origin: string
+  origin: string,
 ) => OriginFunctionResult | Promise<OriginFunctionResult>;
 
 /**
@@ -23,34 +23,34 @@ type OriginFunction = (
  * @template Methods - The allowed HTTP methods, defaults to the common CORS methods.
  */
 export type CorsOptions<Methods extends BaseMethods = BaseMethods> = Partial<{
-	/**
-	 * The origin(s) that are allowed to access the resources. This can be a string, an array of strings,
-	 * or a function that returns a boolean or a promise that resolves to a boolean.
-	 */
-	origin: string | string[] | OriginFunction;
+  /**
+   * The origin(s) that are allowed to access the resources. This can be a string, an array of strings,
+   * or a function that returns a boolean or a promise that resolves to a boolean.
+   */
+  origin: string | string[] | OriginFunction;
 
-	/**
-	 * The HTTP methods that are allowed for CORS requests. Defaults to `["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]`.
-	 */
-	allowMethods: Methods[];
+  /**
+   * The HTTP methods that are allowed for CORS requests. Defaults to `["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]`.
+   */
+  allowMethods: Methods[];
 
-	/**
-	 * The headers that are allowed to be sent in the CORS request.
-	 */
-	allowHeaders: string[];
+  /**
+   * The headers that are allowed to be sent in the CORS request.
+   */
+  allowHeaders: string[];
 
-	/**
-	 * The headers that are exposed to the client in the CORS response.
-	 */
-	exposeHeaders: string[];
+  /**
+   * The headers that are exposed to the client in the CORS response.
+   */
+  exposeHeaders: string[];
 
-	/**
-	 * Whether credentials (cookies, authorization headers, etc.) are allowed in the request.
-	 */
-	credentials: boolean;
+  /**
+   * Whether credentials (cookies, authorization headers, etc.) are allowed in the request.
+   */
+  credentials: boolean;
 
-	/**
-	 * The maximum time (in seconds) that the results of a preflight request can be cached.
-	 */
-	maxAge: number;
+  /**
+   * The maximum time (in seconds) that the results of a preflight request can be cached.
+   */
+  maxAge: number;
 }>;
