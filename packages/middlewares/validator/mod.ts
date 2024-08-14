4d74d5b57092e6505496112f1653fb9f1ef4f899
cat394 <keyring1231@gmail.com>
2024-08-14 12:41:58 +0900

    chore: format code

--- packages/middlewares/validator/mod.ts
+++ packages/middlewares/validator/mod.ts
@@ -1,22 +1,22 @@
-import { validateBody } from './src/body-validator/main.ts';
-import type { ValidatedBodyState } from './src/body-validator/types.ts';
-import { validateQuery } from './src/query-validator/main.ts';
-import type { ValidatedQueryState } from './src/query-validator/types.ts';
-import { CustomStateNames, Symbols } from './src/constants.ts';
+import { validateBody } from "./src/body-validator/main.ts";
+import type { ValidatedBodyState } from "./src/body-validator/types.ts";
+import { validateQuery } from "./src/query-validator/main.ts";
+import type { ValidatedQueryState } from "./src/query-validator/types.ts";
+import { CustomStateNames, Symbols } from "./src/constants.ts";
 import {
-	MomijiInvalidBodyError,
-	MomijiInvalidQueryError,
-	MomijiValidationError,
-} from './src/error-class/main.ts';
+  MomijiInvalidBodyError,
+  MomijiInvalidQueryError,
+  MomijiValidationError,
+} from "./src/error-class/main.ts";
 
 export {
-	Symbols,
-	CustomStateNames,
-	MomijiInvalidBodyError,
-	MomijiInvalidQueryError,
-	MomijiValidationError,
-	validateBody,
-	validateQuery,
-	type ValidatedBodyState,
-	type ValidatedQueryState,
+  CustomStateNames,
+  MomijiInvalidBodyError,
+  MomijiInvalidQueryError,
+  MomijiValidationError,
+  Symbols,
+  validateBody,
+  type ValidatedBodyState,
+  type ValidatedQueryState,
+  validateQuery,
 };
