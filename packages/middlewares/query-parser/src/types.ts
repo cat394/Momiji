1d592154ef8d695a9db6ecf41a8ca249192da452
cat394 <keyring1231@gmail.com>
2024-08-14 12:50:53 +0900

    chore: format code

--- packages/middlewares/query-parser/src/types.ts
+++ packages/middlewares/query-parser/src/types.ts
@@ -2,18 +2,17 @@ import type { CustomStateNames } from "./constants.ts";
 
 /**
  * Used to type-safely access the parsed query parameter state.
- * 
+ *
  * @example
  * import { Application, type Context } from "@oak/oak";
  * import { qureyParser, type ParsedQueryState } from "@momiji/query-parser";
- * 
+ *
  * const app = new Application();
- * 
+ *
  * app.use(queryParser());
- * 
+ *
  * app.use(async (ctx: Context<ParsedQueryState>) => {
  *  const { parsedQuery } = ctx.state;
  * })
- * 
- * */ 
+ */
 export type ParsedQueryState = Record<CustomStateNames.PARSED_QUREY, unknown>;
