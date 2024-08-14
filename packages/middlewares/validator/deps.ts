5cd3667ad84eb283ebe56d82e40ba8c2c78c7b87
cat394 <keyring1231@gmail.com>
2024-08-14 14:00:15 +0900

    chore: format code

--- packages/middlewares/validator/deps.ts
+++ packages/middlewares/validator/deps.ts
@@ -1,46 +1,46 @@
 import {
-	z,
-	ZodError,
-	type ZodIssue,
-	type ZodIssueCode,
-	type ZodObject,
-	type ZodRawShape,
-	type ZodSchema,
-} from 'data:zod';
+  z,
+  ZodError,
+  type ZodIssue,
+  type ZodIssueCode,
+  type ZodObject,
+  type ZodRawShape,
+  type ZodSchema,
+} from "data:zod";
 import {
-	Application,
-	type Context,
-	type Middleware,
-	Router,
-	Status,
-} from '@oak/oak';
-import { assertEquals, assertInstanceOf } from '@std/assert';
-import { assertType, type IsExact } from '@std/testing/types';
-import * as testing from '@scope/shared-test-tools';
-import { type ParsedQueryState, queryParser } from '@momiji/query-parser';
-import type { MergeStates } from '@momiji/utility-types';
-import { createApp } from '@scope/shared-test-tools';
+  Application,
+  type Context,
+  type Middleware,
+  Router,
+  Status,
+} from "@oak/oak";
+import { assertEquals, assertInstanceOf } from "@std/assert";
+import { assertType, type IsExact } from "@std/testing/types";
+import * as testing from "@scope/shared-test-tools";
+import { type ParsedQueryState, queryParser } from "@momiji/query-parser";
+import type { MergeStates } from "@momiji/utility-types";
+import { createApp } from "@scope/shared-test-tools";
 
 export {
-	Application,
-	assertEquals,
-	assertInstanceOf,
-	assertType,
-	type Context,
-	createApp,
-	type IsExact,
-	type MergeStates,
-	type Middleware,
-	type ParsedQueryState,
-	queryParser,
-	Router,
-	Status,
-	testing,
-	z,
-	ZodError,
-	type ZodIssue,
-	type ZodIssueCode,
-	type ZodObject,
-	type ZodRawShape,
-	type ZodSchema,
+  Application,
+  assertEquals,
+  assertInstanceOf,
+  assertType,
+  type Context,
+  createApp,
+  type IsExact,
+  type MergeStates,
+  type Middleware,
+  type ParsedQueryState,
+  queryParser,
+  Router,
+  Status,
+  testing,
+  z,
+  ZodError,
+  type ZodIssue,
+  type ZodIssueCode,
+  type ZodObject,
+  type ZodRawShape,
+  type ZodSchema,
 };
