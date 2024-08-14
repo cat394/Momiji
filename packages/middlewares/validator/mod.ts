d1cfdeb26f4f07c562673994f3703c496c923956
cat394 <keyring1231@gmail.com>
2024-08-14 12:38:07 +0900

    chore: modified the module directory before publishing

--- packages/middlewares/validator/mod.ts
+++ packages/middlewares/validator/mod.ts
@@ -1,20 +1,22 @@
-import { validateBody } from "./src/body-validator/main.ts";
-import { validateQuery } from "./src/query-validator/main.ts";
-import { CustomStateNames, Symbols } from "./src/constants.ts";
-import type { MergeSchemas } from "./src/common-types/main.ts";
+import { validateBody } from './src/body-validator/main.ts';
+import type { ValidatedBodyState } from './src/body-validator/types.ts';
+import { validateQuery } from './src/query-validator/main.ts';
+import type { ValidatedQueryState } from './src/query-validator/types.ts';
+import { CustomStateNames, Symbols } from './src/constants.ts';
 import {
-  MomijiInvalidBodyError,
-  MomijiInvalidQueryError,
-  MomijiValidationError,
-} from "./src/error-class/main.ts";
+	MomijiInvalidBodyError,
+	MomijiInvalidQueryError,
+	MomijiValidationError,
+} from './src/error-class/main.ts';
 
 export {
-  CustomStateNames,
-  type MergeSchemas,
-  MomijiInvalidBodyError,
-  MomijiInvalidQueryError,
-  MomijiValidationError,
-  Symbols,
-  validateBody,
-  validateQuery,
+	Symbols,
+	CustomStateNames,
+	MomijiInvalidBodyError,
+	MomijiInvalidQueryError,
+	MomijiValidationError,
+	validateBody,
+	validateQuery,
+	type ValidatedBodyState,
+	type ValidatedQueryState,
 };
