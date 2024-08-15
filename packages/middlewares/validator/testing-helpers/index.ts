16b4f8f6291177d7861599288d53babc2d48bde4
cat394 <keyring1231@gmail.com>
2024-08-15 11:00:23 +0900

    chore: format code

--- packages/middlewares/validator/testing-helpers/index.ts
+++ packages/middlewares/validator/testing-helpers/index.ts
@@ -1,13 +1,13 @@
 import {
-	t_createParamError,
-	t_createParamTypeErrorMessage,
-} from './create-error-message.ts';
-import { t_zodErrorCatcher } from './zod-error-catcher.ts';
-import { t_createApp } from './createa-app.ts';
+  t_createParamError,
+  t_createParamTypeErrorMessage,
+} from "./create-error-message.ts";
+import { t_zodErrorCatcher } from "./zod-error-catcher.ts";
+import { t_createApp } from "./createa-app.ts";
 
 export {
-	t_createParamError,
-	t_createParamTypeErrorMessage,
-	t_zodErrorCatcher,
-	t_createApp,
+  t_createApp,
+  t_createParamError,
+  t_createParamTypeErrorMessage,
+  t_zodErrorCatcher,
 };
--- packages/middlewares/validator/testing-helpers/zod-error-catcher.ts
+++ packages/middlewares/validator/testing-helpers/zod-error-catcher.ts
@@ -1,13 +1,13 @@
-import { type Middleware, Status } from '../deps.ts';
-import { MomijiValidationError } from '../src/error-class/main.ts';
+import { type Middleware, Status } from "../deps.ts";
+import { MomijiValidationError } from "../src/error-class/main.ts";
 
 export const t_zodErrorCatcher = (): Middleware => async (ctx, next) => {
-	try {
-		await next();
-	} catch (err) {
-		if (err instanceof MomijiValidationError) {
-			ctx.response.status = Status.BadRequest;
-			ctx.response.body = err.zodErrors;
-		}
-	}
+  try {
+    await next();
+  } catch (err) {
+    if (err instanceof MomijiValidationError) {
+      ctx.response.status = Status.BadRequest;
+      ctx.response.body = err.zodErrors;
+    }
+  }
 };
